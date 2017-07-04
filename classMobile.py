9ea889af45084875d78af6d5cbd4e6638707075f
Elijah <elijate@gmail.com>
2017-07-04 16:19:51 +0300

    Create classMobile.py

---
+++ classMobile.py
@@ -0,0 +1,17 @@
+class mobile(object):
+ """this is the mobile class"""
+    def __init__(self):
+        self.IEMICode
+        self.Processor_Type
+        self.simcard
+        self.internalMemory
+        self.IssingleSim
+
+    def GetIEMI(self):
+        return 'IEMI code - IEDAF4312987645'    
+    def dial():
+        return 'Dial a number'
+    def receive(self):
+        return 'Receive a call'
+    def sendMessage(self):
+        return 'Message Sent'  
